96bce3cd658a28bd9dfd2c40d2a2d56b2768a474
taniakrisanty <tania.krisanty@gmail.com>
2022-07-26 15:19:13 +0200

    texture showed up (but maybe the triangles are still inverted)

--- label/video_slicer.cxx
+++ label/video_slicer.cxx
@@ -176,51 +176,42 @@ void video_slicer::draw_oblique_slices(cgv::render::context& ctx)
 
 void video_slicer::draw_oblique_slice(size_t index, cgv::render::context& ctx)
 {
+	std::vector<vec3> P;
+	std::vector<float> O;
+
 	std::vector<vec3> polygon;
-	//construct_slice(index, polygon);
+	construct_slice(index, polygon);
 
-	if (polygon.size() < 3)
+	if (!polygon.empty())
+	{
+		for (int i = polygon.size() - 1; i > 1; --i)
+		{
+			P.push_back(polygon[0]);
+			P.push_back(polygon[i]);
+			P.push_back(polygon[i - 1]);
+			for (int j = 0; j < 3; ++j)
+				O.push_back(1.0f);
+		}
+	}
+
+	if (P.empty())
 		return;
-
-	std::vector<float> normals, vertices;
-	std::vector<int> vertex_indices, tex_coords;
-
-	vec3 a(polygon[0] - polygon[1]);
-	vec3 b(polygon[0] - polygon[2]);
-	vec3 n(cross(a, b));
-
-	for (int i = 0; i < 3; ++i)
-	{
-		normals.push_back(n[i]);
-	}
-
-	int i = 0;
-	for (auto point : polygon)
-	{
-		vertices.push_back(point.x());
-		vertices.push_back(point.y());
-		vertices.push_back(point.z());
-
-		vertex_indices.push_back(0);
-		tex_coords.push_back(i++);
-	}
 
 	GLboolean is_culling;
 	glGetBooleanv(GL_CULL_FACE, &is_culling);
 	glDisable(GL_CULL_FACE);
-	//aam.set_attribute_array(ctx, slice_prog.get_attribute_location(ctx, "position"), P);
-	//aam.set_attribute_array(ctx, slice_prog.get_attribute_location(ctx, "opacity"), O);
-	//aam.enable(ctx);
+	aam.set_attribute_array(ctx, slice_prog.get_attribute_location(ctx, "position"), P);
+	aam.set_attribute_array(ctx, slice_prog.get_attribute_location(ctx, "opacity"), O);
+	aam.enable(ctx);
 	vol_tex.enable(ctx, 0);
 	slice_prog.enable(ctx);
 	slice_prog.set_uniform(ctx, "box_min_point", position - 0.5f * V.get_extent());
 	slice_prog.set_uniform(ctx, "box_extent", V.get_extent());
 	slice_prog.set_uniform(ctx, "vol_tex", 0);
-	//glDrawArrays(GL_TRIANGLES, 0, GLsizei(P.size()));
-	//ctx.draw_faces(vertices.data(), normals.data(), 0, tex_coords.data(), vertex_indices.data(), tex_coords.data(), 1, polygon.size());
+	glDrawArrays(GL_TRIANGLES, 0, GLsizei(P.size()));
 	slice_prog.disable(ctx);
 	vol_tex.disable(ctx);
-	//aam.disable(ctx);
+	aam.disable(ctx);
 	if (is_culling)
 		glEnable(GL_CULL_FACE);
 }
@@ -254,9 +245,8 @@ void video_slicer::construct_slice(size_t index, std::vector<vec3>& polygon) con
 				   polygon. Store the ordered edge points in the polygon-vector. Create your own
 				   helper structures for edge-face adjacenies etc.*/
 
-	static int a_corner_comparisons[12] = { 0, 3, 7, 4, 0, 4, 5, 1, 0, 1, 2, 3 };
-	static int b_corner_comparisons[12] = { 1, 2, 6, 5, 3, 7, 6, 2, 4, 5, 6, 7 };
-	static int comparison_to_edges[12] = { 0, 2, 6, 4, 3, 7, 5, 1, 8, 9, 10, 11 };
+	static int a_corner_comparisons[12] = { 0, 2, 6, 4, 0, 4, 5, 1, 0, 1, 3, 2 };
+	static int b_corner_comparisons[12] = { 1, 3, 7, 5, 2, 6, 7, 3, 4, 5, 7, 6 };
 
 	std::vector<vec3> p;
 
@@ -275,7 +265,7 @@ void video_slicer::construct_slice(size_t index, std::vector<vec3>& polygon) con
 
 			coord(i / 4) = new_value;
 
-			p.push_back(voxel_to_world_coordinate_transform(coord));
+			p.push_back(coord);
 		}
 	}
 
@@ -285,7 +275,7 @@ void video_slicer::construct_slice(size_t index, std::vector<vec3>& polygon) con
 	vec3 p0 = p[0];
 	p.erase(p.begin());
 
-	polygon.push_back(p0);
+	polygon.push_back(voxel_to_world_coordinate_transform(p0));
 
 	while (p.size() > 1)
 	{
@@ -293,7 +283,9 @@ void video_slicer::construct_slice(size_t index, std::vector<vec3>& polygon) con
 
 		for (unsigned int i = 0; i < 3; ++i)
 		{
-			if (p0(i) < std::numeric_limits<float>::epsilon() || p0(i) > 1 - std::numeric_limits<float>::epsilon())
+			//if (p0(i) < std::numeric_limits<float>::epsilon() || p0(i) > 1 - std::numeric_limits<float>::epsilon())
+			if (fabs(p0(i) - B.get_min_pnt()(i)) < std::numeric_limits<float>::epsilon() ||
+				fabs(p0(i) - B.get_max_pnt()(i)) < std::numeric_limits<float>::epsilon())
 			{
 				for (unsigned int j = 0; j < p.size(); ++j)
 				{
@@ -303,7 +295,7 @@ void video_slicer::construct_slice(size_t index, std::vector<vec3>& polygon) con
 					{
 						p.erase(p.begin() + j);
 
-						polygon.push_back(f);
+						polygon.push_back(voxel_to_world_coordinate_transform(f));
 
 						p0 = f;
 						found = true;
@@ -316,7 +308,7 @@ void video_slicer::construct_slice(size_t index, std::vector<vec3>& polygon) con
 		}
 	}
 
-	polygon.push_back(p[0]);
+	polygon.push_back(voxel_to_world_coordinate_transform(p[0]));
 }
 float video_slicer::signed_distance_from_slice(size_t index, const vec3& p) const
 {
